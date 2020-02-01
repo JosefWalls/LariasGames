f44bd9c5599a9f3298abeb64dca05dc5d99f14b0
Josef Walls <josef.l.walls@gmail.com>
2020-02-01 16:19:43 -0600

    Updates on scenario pages

--- src/components/Scenario.js
+++ src/components/Scenario.js
@@ -21,17 +21,57 @@ class Scenario extends React.Component {
                     <header>
                         <h1>{val.Title}</h1>
                     </header>
+                        <section className="mappedScenarioImages">
+                        {
+                            val.Images.map((val, i) => {
+                                return (
+                                    <div>
+                                        <img src={val}></img>
+                                    </div>
+                                )
+                            })
+                        }
+                        </section>
                     <h6 id="publisher">Published By: {val.Publisher}</h6>
                     <p id="price">${val.Price}</p>
                     <p id="productId">Product ID: {val._id}</p>
-                    <h3>Description:</h3>
-                    <p>{val.Description}</p>
+                    <section>
+                        <article>
+                            <header>
+                                <h3>Description:</h3>
+                            </header>
+                            <p>{val.Description}</p>
+                        </article>
+                        <article>
+                            <header>
+                                <h3>Included Scenarios</h3>
+                            </header>
+                            <section id="mappedFeaturesCard">
+                            {
+                            val.Features.map((val, i) => {
+                                return (
+                                    <div className="mappedFeatures">
+                                        <p>{val}</p>
+                                    </div>
+                                )
+                            })
+                            }
+                            </section>
+                        </article>
+                    </section>
+                    <footer>
+                        <a href={val.Link} target="_blank">
+                            <button>Purchase Scenario Pack</button>
+                        </a>
+                    </footer>
                 </div>
             )
         })
         return (
             <div className="scenarioCard">
-                {mappedInfo}
+                <section>
+                    {mappedInfo}
+                </section>
             </div>
         )
     }
