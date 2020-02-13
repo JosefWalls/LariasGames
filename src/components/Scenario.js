863a5038ba445be7c27b23f189eb043455c529e2
Josef Walls <josef.l.walls@gmail.com>
2020-02-13 12:16:00 -0600

    Fixed scenario import

--- src/components/Scenario.js
+++ src/components/Scenario.js
@@ -2,7 +2,7 @@ import React from "react";
 import {connect} from "react-redux";
 import {getScenario} from "./../redux/reducers/Product";
 import "./Scenario.css";
-import {Carousel, Card} from "react-bootstrap";
+import {Carousel, Card, Jumbotron, Container} from "react-bootstrap";
 import Nav from "./Navbar";
 
 class Scenario extends React.Component {
